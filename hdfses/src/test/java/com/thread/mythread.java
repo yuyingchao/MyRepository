aaae35f24f307d8815db496358d4c9edf5843283
yuyingchao <yuyingchao1994>
2016-12-05 15:03:59 +0800

    更改后

--- hdfses/src/test/java/com/thread/mythread.java
+++ hdfses/src/test/java/com/thread/mythread.java
@@ -1,4 +1,10 @@
 package com.thread;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.collections.functors.IfClosure;
+
 /**
  * 2016-12-05
  * @author yyc
@@ -6,15 +12,25 @@ package com.thread;
  */
 public class mythread {
 public static void main(String[] args) {
+	final ScheduledThreadPoolExecutor poolExecutor = new ScheduledThreadPoolExecutor(10);
 	
-	
-	
+	poolExecutor.scheduleAtFixedRate(new Runnable() {
+		int count =0;
+		public void run() {
+			System.out.println("Ïß³Ì"+Thread.currentThread().getName());
+			count ++;
+			if(count > 10){
+				poolExecutor.shutdown();
+			}
+		}
+		
+	}
+	, 100, 200, TimeUnit.MILLISECONDS);
 	
 	
 }
-	
-	
-	
+
+
 	
 	
 	
